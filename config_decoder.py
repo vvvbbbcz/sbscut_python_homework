70d0e769fbd85c5dad6aec4f66a23332c145f364
vvvbbbcz <vvvbbbcz@bzgzs.cn>
2025-04-18 15:29:56 +0800

    feat: config structure

--- config_decoder.py
+++ config_decoder.py
@@ -1,7 +1,7 @@
 import json
 
 
-class Config:
+class __Config:
 	def __init__(self):
 		config_file = open("config.json", "r")
 		config_dict: dict = json.load(config_file)
@@ -11,9 +11,10 @@ class Config:
 		self.cookies: dict = config_dict.get("cookies", {})
 		self.user_agent: str = config_dict.get("user_agent", "")
 		self.ds_api_key: str = config_dict.get("deepseek_api_key", "")
-		self.threads: int = config_dict.get("threads", 1)
+		self.ai_threads: int = config_dict.get("ai_threads", 1)
+		self.running_threads: int = config_dict.get("running_threads", 1)
 		self.start_question: int = config_dict.get("start_question", 0)
 		self.log_level: str = config_dict.get("log_level", "INFO")
 
 
-config: Config = Config()
+config: __Config = __Config()
